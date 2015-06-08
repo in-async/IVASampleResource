ac49a7fe9fb4aee6f296abe62b9c44e0a8aae7b9
in-async <inasync@gmail.com>
2015-06-08 18:37:06 +0900

    更新

--- player.debug.js
+++ player.debug.js
@@ -1,4 +1,9 @@
-﻿{
+﻿(function () {
+    var zoneEid = 'zone3';
+    var campaignId = 3;
+    var creativeId = 113;
+    var creativePath = '';
+    var mediaJson = {
         "default_zone_eid": "zone2",
         "settings": {
             "log_click_url": "http://192.168.1.10:8003/api/log_click",
@@ -90,4 +95,87 @@
                 "zone_id": 3
             }
         ]
-    }
+    };
+    var duration = 3.0;
+    var video;
+
+    // 疑似イベント励起
+    window.addEventListener('load', function () {
+        native_onLoadAd(zoneEid, campaignId, creativeId, creativePath, mediaJson);
+
+        // 疑似動画再生準備
+        (function () {
+            video = document.createElement('video');
+            video.setAttribute('src', _creative.video_url);
+            //video.setAttribute('autoplay', 'autoplay');
+            video.setAttribute('style', 'position:absolute; width:100%; height:100%; top:0; left:0; background-color:#000');
+
+            // 動画のイベントハンドラ設定
+            video.addEventListener('play', function () {
+                console.log('video.play');
+                native_onStartedAd(zoneEid);
+            });
+            video.addEventListener('pause', function () {
+                console.log('video.pause');
+            });
+            video.addEventListener('ended', function () {
+                console.log('video.ended');
+                native_onFinishedAd(zoneEid, video.duration, false);
+            })
+            video.addEventListener('timeupdate', function () {
+                native_onUpdateTime(video.currentTime, video.duration);
+            });
+
+            document.body.appendChild(video);
+
+            // フレームが video より前面になるよう配慮
+            //document.getElementById('video-frame').style.position = 'relative';
+            //document.getElementById('video-frame').style.zIndex = 100;
+            //document.getElementById('endcard-frame').style.position = 'relative';
+            //document.getElementById('endcard-frame').style.zIndex = 100;
+        })();
+    });
+    /**
+     * debug 用スタブメソッド
+     */
+    var currentTime = 0;
+    var elapsed = 0;
+    var skipped = false;
+    var intervalId;
+    nativeApi = function (method, jsonArgs, callback) {
+        switch (method) {
+            case 'startVideo':
+                video.play();
+                break;
+            case 'pauseVideo':
+                video.pause();
+                break;
+            case 'skipAd':
+                video.pause();
+                native_onFinishedAd(zoneEid, video.currentTime, true);
+                break;
+            case 'openClickUrl':
+                window.open(jsonArgs.url);
+                break;
+            case 'closeAd':
+                break;
+            case 'getCreativeAsBase64':
+                setTimeout(function () {
+                    callback(jsonArgs.creativeUrl);
+                }, 0);
+                break;
+        }
+    };
+
+    //// native_onFailed イベントボタン
+    //(function () {
+    //    var btn = document.createElement('button');
+    //    btn.innerHTML = 'native_onFailed';
+    //    btn.addEventListener('click', function () {
+    //        var reason = 1;
+    //        native_onFailed(reason, zoneEid);
+    //    });
+    //    document.body.appendChild(btn);
+    //})();
+})();
+
