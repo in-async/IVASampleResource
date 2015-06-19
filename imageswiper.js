86ef020a5ebe64a897647e1936aacab2fe29b934
kashimura <kashimura@i-mobile.co.jp>
2015-06-19 18:05:38 +0900

    試作

--- imageswiper.js
+++ imageswiper.js
@@ -5,73 +5,64 @@
 /**
  * 複数画像をスワイプする為のクラス
  */
-var ImageSwiper = (function (imageContainer, options) {
+var ImageSwiper = (function (imageSwiper, options) {
     var thisObj = this;
 
     // 引数の検証
-    imageContainer.className += ' image-swiper';
+    imageSwiper.className += ' image-swiper';
     if (!options) {
         options = {};
     }
     if (!options.imageTagName) {
         options.imageTagName = 'div';
     }
-    if (typeof(options.colStripeVisible) === 'undefined') {
-        options.colStripeVisible = true;
-    }
 
     // 現在表示されている画像インデックス
     var _currentIndex = -1;
     // スワイプ対象となる画像要素配列
     var _imageElements = [];
-    // 縦帯要素
-    var _leftStripeElement;
-    var _rightStripeElement;
-    if (options.colStripeVisible) {
-        // 縦帯要素の追加
-        var _leftStripeElement = document.createElement('span');
-        _leftStripeElement.setAttribute('class', 'left-stripe');
-        imageContainer.appendChild(_leftStripeElement);
 
-        var _rightStripeElement = document.createElement('span');
-        _rightStripeElement.setAttribute('class', 'right-stripe');
-        imageContainer.appendChild(_rightStripeElement);
-    }
-
+    //var imageContainer = document.createElement('div');
+    //imageContainer.setAttribute('class', 'container');
+    //imageSwiper.appendChild(imageContainer);
+    var imageContainer = imageSwiper;
 
     /**
      * 画像 URL を追加します。
      */
     this.addImageUrl = function (imageUrl) {
         // 画像要素の作成
-        var imageElem = document.createElement(options.imageTagName);
-        imageElem.style.backgroundImage = 'url("' + imageUrl + '")';
+        var imageElem = document.createElement('img');
+        imageElem.src = imageUrl;
         imageContainer.appendChild(imageElem);
 
         // 画像要素の追加
         thisObj.addImageElement(imageElem);
     };
 
+    var _containerWidth = 0;
     /**
      * 画像要素を追加します。
      */
     this.addImageElement = function (imageElem) {
         console.log(imageElem);
+        //var offsetWidth = imageElem.offsetWidth;
+        //imageElem.style.left = _containerWidth + 'px';
+        //_containerWidth += offsetWidth;
         _imageElements.push(imageElem);
 
         // 現在のスワイプ画像インデックスが負値だった場合、最初のスワイプ画像を表示状態にする
         if (_currentIndex < 0) {
             thisObj.setIndex(0);
         }
-
-        // 縦帯のリフレッシュ
-        refreshColStripe();
     };
 
+    var _autoScrollTimerId;
     /**
      * 表示対象のスワイプ画像のインデックスを変更します。
      */
-    this.setIndex = function (index) {
+    this.setIndex = function (index, animation) {
+        console.log('setIndex');
         // 指定したスワイプ画像インデックスが画像配列数を上回っていたら、代わりに最後尾を指定
         if (index >= _imageElements.length) {
             index = _imageElements.length - 1;
@@ -85,36 +76,83 @@ var ImageSwiper = (function (imageContainer, options) {
         if (_currentIndex < 0) return;
 
         if (indexChanged) {
-            // 全ての画像要素のクラスを初期化
-            for (var i = 0, len = _imageElements.length; i < len; i++) {
-                if (i === index) {
-                    _imageElements[index].className = 'fadeIn';
-                } else {
-                    _imageElements[i].className = '';
-                }
-            }
-
-            // 縦帯のリフレッシュ
-            refreshColStripe();
+            //// 全ての画像要素のクラスを初期化
+            //for (var i = 0, len = _imageElements.length; i < len; i++) {
+            //    if (i === index) {
+            //        _imageElements[index].className = 'fadeIn';
+            //    } else {
+            //        _imageElements[i].className = '';
+            //    }
+            //}
+            //imageContainer.style.left = -imgElem.offsetLeft + 'px';
         }
+        var imgElem = _imageElements[_currentIndex];
+
+        if (animation) {
+            (function () {
+                var fromLeft = imageSwiper.scrollLeft;
+                var toLeft = imgElem.offsetLeft - imageSwiper.clientWidth / 2 + imgElem.offsetWidth / 2;
+                var duration = 200;
+                var interval = 10;
+                var count = duration / interval;
+                var step = (toLeft - fromLeft) / count;
+                var ti = 0;
+                clearTimeout(_autoScrollTimerId);
+                _autoScrollTimerId = setInterval(function () {
+                    if (++ti < count) {
+                        imageSwiper.scrollLeft += step;
+                    } else {
+                        clearTimeout(_autoScrollTimerId);
+                        _autoScrollTimerId = null;
+                        _isAutoScrolling = false;
+                        imageSwiper.scrollLeft = toLeft;
+                    }
+                }, interval);
+            })();
+        } else {
+            imageSwiper.scrollLeft = imgElem.offsetLeft - imageSwiper.clientWidth / 2 + imgElem.offsetWidth / 2;
+        }
+
+        for (var i in _imageElements) {
+            _imageElements[i].removeAttribute('class');
+        }
+        imgElem.setAttribute('class', 'active');
     };
 
-    /**
-     * 縦帯の表示リフレッシュ
-     */
-    function refreshColStripe() {
-        if (_leftStripeElement) {
-            _leftStripeElement.style.display = (_currentIndex == 0) ? 'none' : 'block';
+    //var _scrollTimerId;
+    //imageContainer.addEventListener('scroll', function () {
+    //    console.log('scroll');
+    //    clearTimeout(_scrollTimerId);
+    //    _scrollTimerId = setTimeout(function () {
+    //        if (_autoScrollTimerId) {
+    //            clearTimeout(_autoScrollTimerId);
+    //            _autoScrollTimerId = null;
+    //        }
+
+    //        var curElem = getCenterViewElement();
+    //        var index = _imageElements.indexOf(curElem);
+    //        thisObj.setIndex(index, true);
+    //    }, 50);
+    //});
+
+    function getCenterViewElement() {
+        var centerX = imageContainer.scrollLeft + imageContainer.clientWidth / 2;
+        for (var i = 0, len = _imageElements.length; i < len; i++) {
+            var imgElem = _imageElements[i];
+            var offsetLeft = imgElem.offsetLeft;
+            var offsetRight = offsetLeft + imgElem.offsetWidth;
+            if (centerX > offsetLeft && centerX < offsetRight) {
+                return imgElem;
+            }
         }
-        if (_rightStripeElement) {
-            _rightStripeElement.style.display = (_currentIndex == _imageElements.length - 1) ? 'none' : 'block';
-        }
+        return null;
     }
 
     (function () {
         /**
          * スワイプ中に使用する内部フィールド
          */
+        var touchStartScrollLeft = null;
         var touchStartX = null;
         var touchMoveX = null;
         var containerWidth = null;
@@ -132,6 +170,7 @@ var ImageSwiper = (function (imageContainer, options) {
          * スワイプ開始イベント
          */
         function onTouchStart() {
+            console.log('onTouchStart');
             // フィールド初期化
             touchStartX = touchMoveX = null;
             curElem = prevElem = nextElem = null;
@@ -139,26 +178,27 @@ var ImageSwiper = (function (imageContainer, options) {
             // コンテナ幅を保持
             containerWidth = imageContainer.offsetWidth;
             // タッチ位置を保持
-            touchStartX = event.touches ? event.touches[0].pageX : event.pageX;
+            touchStartX = touchMoveX = event.touches ? event.touches[0].pageX : event.pageX;
+            touchStartScrollLeft = imageContainer.scrollLeft;
 
-            var prevIndex = (_currentIndex === 0) ? _imageElements.length - 1 : _currentIndex - 1;
-            var nextIndex = (_currentIndex + 1 >= _imageElements.length) ? 0 : _currentIndex + 1;
+            //var prevIndex = (_currentIndex === 0) ? _imageElements.length - 1 : _currentIndex - 1;
+            //var nextIndex = (_currentIndex + 1 >= _imageElements.length) ? 0 : _currentIndex + 1;
 
-            // 対象となる画像要素とその前後要素を保持
-            if (_imageElements.length > 2) {
-                prevElem = _imageElements[prevIndex];
-                prevElem.className = 'swipe';
-                prevElem.style.left = -containerWidth + 'px';
-            }
-            curElem = _imageElements[_currentIndex];
-            curElem.style.left = 0;
-            curElem.className = 'swipe';
-            if (true) {
-                nextElem = _imageElements[nextIndex];
-                nextElem.className = 'swipe';
-                nextElem.style.left = containerWidth + 'px';
-            }
-            console.log({ prevElem: prevElem, curElem: curElem, nextElem: nextElem });
+            //// 対象となる画像要素とその前後要素を保持
+            //if (_imageElements.length > 2) {
+            //    prevElem = _imageElements[prevIndex];
+            //    prevElem.className = 'swipe';
+            //    prevElem.style.left = -containerWidth + 'px';
+            //}
+            //curElem = _imageElements[_currentIndex];
+            //curElem.style.left = 0;
+            //curElem.className = 'swipe';
+            //if (true) {
+            //    nextElem = _imageElements[nextIndex];
+            //    nextElem.className = 'swipe';
+            //    nextElem.style.left = containerWidth + 'px';
+            //}
+            //console.log({ prevElem: prevElem, curElem: curElem, nextElem: nextElem });
 
             // タッチ・マウスイベントを登録
             // register touchmove / mousemove
@@ -181,71 +221,72 @@ var ImageSwiper = (function (imageContainer, options) {
             // スワイプ移動量の算出
             var dx = (touchMoveX - touchStartX);
             console.log({ dx: dx });
-            if (dx > 0 && prevElem || dx < 0 && nextElem) {
-            } else {
-                // 範囲外へのスワイプはバウンド挙動に
-                dx *= .3;
-            }
+            //if (dx > 0 && _currentIndex > 0 || dx < 0 && _currentIndex < _imageElements.length - 1) {
+            //} else {
+            //    // 範囲外へのスワイプはバウンド挙動に
+            //    dx *= .3;
+            //}
 
-            // スワイプ移動量分、画像要素を移動
-            if (prevElem) {
-                prevElem.style.left = -containerWidth + dx + 'px';
-            }
-            curElem.style.left = dx + 'px';
-            if (nextElem) {
-                nextElem.style.left = containerWidth + dx + 'px';
-            }
+            //// スワイプ移動量分、画像要素を移動
+            //if (prevElem) {
+            //    prevElem.style.left = -containerWidth + dx + 'px';
+            //}
+            //curElem.style.left = dx + 'px';
+            //if (nextElem) {
+            //    nextElem.style.left = containerWidth + dx + 'px';
+            //}
+            imageContainer.scrollLeft = touchStartScrollLeft - dx;
         }
         /**
          * スワイプ終了イベント
          */
         function onTouchEnd() {
-            //event.preventDefault();
-            // 移動なしにタッチが終了した場合、当たり前だが移動なしとする
-            if (touchMoveX === null) {
-                touchMoveX = touchStartX;
-            }
+            console.log('onTouchEnd');
+            ////event.preventDefault();
 
-            // 移動量の判定
-            var indexChanged = false;
-            if (touchStartX > touchMoveX) {
-                if (touchStartX > (touchMoveX + 50)) {
-                    //右から左に指が移動した場合
-                    console.log({ msg: 'swipe ←', touchStartX: touchStartX, touchMoveX: touchMoveX });
-                    if (nextElem) {
-                        ++_currentIndex;
-                        if (_currentIndex >= _imageElements.length) {
-                            _currentIndex = 0;
-                        }
-                        nextElem.className = 'fadeIn';
-                        //curElem.className = 'fadeOut';
-                        indexChanged = true;
-                    }
-                }
-            } else if (touchStartX < touchMoveX) {
-                if ((touchStartX + 50) < touchMoveX) {
-                    //左から右に指が移動した場合
-                    console.log({ msg: 'swipe →', touchStartX: touchStartX, touchMoveX: touchMoveX });
-                    if (prevElem) {
-                        --_currentIndex;
-                        if (_currentIndex < 0) {
-                            _currentIndex = _imageElements.length - 1;
-                        }
-                        prevElem.className = 'fadeIn';
-                        //curElem.className = 'fadeOut';
-                        indexChanged = true;
-                    }
-                }
-            }
-            console.log({ _currentIndex: _currentIndex });
+            //// 移動量の判定
+            //var indexChanged = false;
+            //if (touchStartX > touchMoveX) {
+            //    if (touchStartX > (touchMoveX + 50)) {
+            //        //右から左に指が移動した場合
+            //        console.log({ msg: 'swipe ←', touchStartX: touchStartX, touchMoveX: touchMoveX });
+            //        if (nextElem) {
+            //            ++_currentIndex;
+            //            if (_currentIndex >= _imageElements.length) {
+            //                _currentIndex = 0;
+            //            }
+            //            nextElem.className = 'fadeIn';
+            //            //curElem.className = 'fadeOut';
+            //            indexChanged = true;
+            //        }
+            //    }
+            //} else if (touchStartX < touchMoveX) {
+            //    if ((touchStartX + 50) < touchMoveX) {
+            //        //左から右に指が移動した場合
+            //        console.log({ msg: 'swipe →', touchStartX: touchStartX, touchMoveX: touchMoveX });
+            //        if (prevElem) {
+            //            --_currentIndex;
+            //            if (_currentIndex < 0) {
+            //                _currentIndex = _imageElements.length - 1;
+            //            }
+            //            prevElem.className = 'fadeIn';
+            //            //curElem.className = 'fadeOut';
+            //            indexChanged = true;
+            //        }
+            //    }
+            //}
+            //console.log({ _currentIndex: _currentIndex });
 
-            // インデックスに変更が無ければ、スライドを元に戻す
-            if (!indexChanged) {
-                curElem.className = 'fadeIn';
-            } else {
-                // 縦帯のリフレッシュ
-                refreshColStripe();
-            }
+            //// インデックスに変更が無ければ、スライドを元に戻す
+            //if (!indexChanged) {
+            //    curElem.className = 'fadeIn';
+            //} else {
+            //    // 縦帯のリフレッシュ
+            //    refreshColStripe();
+            //}
+            var curElem = getCenterViewElement();
+            var index = _imageElements.indexOf(curElem);
+            thisObj.setIndex(index, true);
 
             // タッチ・マウスイベントを解除
             // unregister touchmove / mousemove
