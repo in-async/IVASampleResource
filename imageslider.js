1c321c6ef04b2ab0ffa586178ebbabdbe2920f08
in-async <inasync@gmail.com>
2015-06-20 15:39:39 +0900

    少なくとも iOS 6 にてローテーション時に画像が不適切な大きさにリサイズ表示されてしまう不具合ので、その対処

--- imageslider.js
+++ imageslider.js
@@ -389,16 +389,3 @@ function scrollLeftMomentum(target, vx, duration, completionCallback) {
     }, ANIMATION_INTERVAL);
     return timerId;
 }
-
-window.document.addEventListener('orientationchange', function () {
-    var iOS = navigator.userAgent.match(/(iPad|iPhone|iPod)/g);
-    var viewportmeta = document.querySelector('meta[name="viewport"]');
-    if (iOS && viewportmeta) {
-        //if (viewportmeta.content.match(/width=device-width/)) {
-            viewportmeta.content = viewportmeta.content.replace(/width=[^,]+/, 'width=1');
-        //}
-        viewportmeta.content = viewportmeta.content.replace(/width=[^,]+/, 'width=' + window.innerWidth);
-    }
-    // If you want to hide the address bar on orientation change, uncomment the next line
-    // window.scrollTo(0, 0);
-}, false);
